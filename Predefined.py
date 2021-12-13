9463e1bebb240649b5e04782d75a8225294b4665
wangmenghao <505479936@qq.com>
2021-12-14 00:34:03 +0800

    Predefined dict added

--- Predefined.py
+++ Predefined.py
@@ -3,20 +3,28 @@ Predefined = {
                 'phrase':'Symbolic Z-matrix:',
                 'indent':2,
                 'index':0
-
     },
+
     'Coordinates':{
                 'phrase':'Standard orientation',
                 'indent':2,
     },
+
     'MullikenCharges':{
                 'phrase':'Mulliken charges:',
                 'indent':2,
                 'index':2
     },
+
     'NBOCHarges':{
                 'phrase':'Summary of Natural Population Analysis:',
                 'indent':6,
+                'index':2
+    },
+
+    'Dipoles':{
+                'phrase':'',
+                'indent':2,
                 'index':2
     }
 }
